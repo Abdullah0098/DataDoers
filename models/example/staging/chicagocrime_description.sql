428e39bea66315e67d40796e3690e98f1210e3dc
Abdullah Najeeb <abdullahnajeeb0306@gmail.com>
2023-05-26 05:02:51 +0000

    NEW CHANGES

--- models/example/staging/chicagocrime_description.sql
+++ models/example/staging/chicagocrime_description.sql
@@ -3,7 +3,7 @@
 with 
 CHICAGOCRIME_DESCRIPTION AS
 (
-    select id  as description_id,description as crime_description from {{ source('src_chicagocrimedata', 'CHICAGOCRIMEDATA') }}
+    select id as description_id,description as crime_description from {{ source('src_chicagocrimedata', 'CHICAGOCRIMEDATA') }}
 )
 
 select * from CHICAGOCRIME_DESCRIPTION
--- models/example/staging/chicagocrime_details.sql
+++ models/example/staging/chicagocrime_details.sql
@@ -4,3 +4,5 @@ with chicagocrime_details as
 (
     select id,case_number,date from {{ source('src_chicagocrimedata', 'CHICAGOCRIMEDATA') }}
 )
+
+select * from chicagocrime_details
